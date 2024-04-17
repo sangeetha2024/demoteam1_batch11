a923fc0441849cdb8c7d3ce4351d88b695b3af17
sande <sandesanthosh88@gmail.com>
2024-04-17 20:19:37 +0530

    new file problem_solving.py

---
+++ python programming - santhosh/problem_solving.py
@@ -0,0 +1,16 @@
+# to find if the given number is prime or not
+
+def isprime(num):
+    if num > 1:
+        for i in range(2, (num//2)+1):
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
+isprime(2)
+print(2%2)
