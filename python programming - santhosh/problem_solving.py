08200a8f2e836a4955f23840e34cbf2b3909de20
sande <sandesanthosh88@gmail.com>
2024-04-18 18:25:54 +0530

    2nd problem

--- python programming - santhosh/problem_solving.py
+++ python programming - santhosh/problem_solving.py
@@ -3,13 +3,33 @@
 def isprime(num):
     if num > 1:
         for i in range(2, (num//2)+1):
-            # If num is divisible by any number between
-            # 2 and n / 2, it is not prime
+
             if (num % i) == 0:
+                
                 print(num, "is not a prime number")
                 break
         else:
             print(num, "is a prime number")
     else:
         print(num, "is not a prime number")
-isprime(2)
+isprime(3)
+
+# Non repeating elements in an array
+
+def nonrepeatingelem(arr,n):
+    for i in range(n):
+        j = 0
+        while(j < n):
+            if (i != j and arr[i] == arr[j]):
+                break
+            j += 1
+        if (j == n):
+            return arr[i]
+ 
+    return -1
+ 
+ 
+arr = [9, 4, 9, 7, 7, 4,3]
+n = len(arr)
+print(nonrepeatingelem(arr, n))
+
